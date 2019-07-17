5ea86412e8be81b9799aa4eeafd1174cc4396afd
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-17 23:12:30 +0530

    Add redirect route to thermal homepage

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 
 module.exports = (req, res) => {
   app.post('/discord', function (req, res) {
+  app.get('/', (req, res) => {
+    res.redirect('https://thermal.codecarrot.net/')
+  });
+
     console.log('Posting message to Discord');
     console.log(res);
   })
