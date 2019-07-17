58a96ec8a619eaf16f505c7c8085b494ed0fb061
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-17 23:12:53 +0530

    Change to es6 syntax

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 module.exports = (req, res) => {
-  app.post('/discord', function (req, res) {
   app.get('/', (req, res) => {
     res.redirect('https://thermal.codecarrot.net/')
   });
 
+  app.post('/discord', (req, res) => {
     console.log('Posting message to Discord');
     console.log(res);
   })
